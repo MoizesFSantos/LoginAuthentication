4416b052c4d94d22295c141c5891311f24cddf2f
Moizes Ferreira dos Santos <moizesferreira66@hotmail.com>
2021-10-19 20:52:16 -0300

    signIn in working

--- src/api/signin.js
+++ src/api/signin.js
@@ -2,9 +2,22 @@ const express = require("express");
 const { route } = require(".");
 const Users = require("../models/users");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
-router.post('signin', (req, res) =>{
+router.post('/signin', async (req, res) =>{
+    const {email, password} = req.body;
+    const userWithEmail = await Users.findOne({where:{email}}).catch((error) =>{
+        console.log(`Error: ${error}`)
+    });
+    if(!userWithEmail){
+        return res.status(400).json({ message: "Email or Password does not match"})
+    };
+    if(userWithEmail.password !== password){
+        return res.status(400).json({ message: "Email or Password does not match"})
+    }
+    const jwtToken = jwt.sign({id: userWithEmail.id, email: userWithEmail.email}, process.env.JWT_SECRET);
 
+    res.json({ message: 'Welcome back!', token: jwtToken});
 })
 
 module.exports = router;
